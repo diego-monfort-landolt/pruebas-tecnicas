825e1233f3ebeab8fa0fc48cad051df57b530dbd
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-09-22 20:17:25 +0200

    add new base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https:///diego-monfort-landolt.github.io/book-library/'
+  base: 'https://diego-monfort-landolt.github.io/book-library/'
 })
